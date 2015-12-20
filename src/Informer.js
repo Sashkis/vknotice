b205ab785f26fa93c92ae963bb1251236dc10eeb
Alex Kozack <cawa-93@yandex.ru>
2015-12-21 00:03:23 +0200

    Функции getLangCode и playSound вынесены из глобального объекта

--- src/Informer.js
+++ src/Informer.js
@@ -1,6 +1,6 @@
 /* globals document, $, chrome, navigator, Vk, App, console, getCase, setTimeout*/
 /*jshint esnext: true */
-/*jshint -W097*/
+/*jshint -W097, -W030*/
 
 "use strict";
 /**
@@ -15,31 +15,6 @@ var Informer = (function () {
 		badge: 0,
 		firstRequest: $.Deferred(),
 
-		/**
-		 * Вычисляет код языка
-		 * @param  {Number}	lang_code	Код языка для проверки
-		 * @return {Number}				Код поддерживаемого языка
-		 */
-		getLangCode: function (lang_code) {
-			switch (lang_code) {
-				case 0: case 97: case 100: case 777:
-					return 0;  // Русский
-				case 1:
-					return 1;  // Украинский
-				case 2: case 114:
-					return 2;  // Белоруский
-				case 6:
-					return 6;  // Немецкий
-				case 15:
-					return 15; // Польский
-				case 54: case 66:
-					return 54; // Румунский
-				case 61:
-					return 61; // Нидерландский
-				default:
-					return 3;  // Английский
-			}
-		},
 
 		/**
 		 * Запуск демона
@@ -61,7 +36,7 @@ var Informer = (function () {
 		 * @returns {Boolean}	Был ли демон остановлен
 		 */
 		deamonStop: function () {
-			if (this.delay < 1) {
+			if (!this.delay) {
 				console.info('Daemon already stopped');
 				return false;
 			}
@@ -78,39 +53,32 @@ var Informer = (function () {
 		mainRequest: function () {
 			$.when(new App().load({
 				'options' : 'friends,photos,videos,messages,groups,notifications',
-				'isLoadComment' : 0,
-				'lastOpenComment' : 0,
-				'lastLoadAlert' : 0,
-			}), new Vk().load()).then((params, vk) => {
-				vk.api('execute.getdata_beta', {
-					'options': params.options,
-					'isLoadComment': params.isLoadComment,
-					'lastOpenComment': params.lastOpenComment,
-					'user_id': vk.user_id,
-				}).done((API) => {
-					if ( this.delay > 0 ) {
-						API.lang = this.getLangCode(API.lang);
+				'isLoadComment': 0,
+				'lastOpenComment': 0,
+				'lastLoadAlert': 0,
+			}), new Vk().load()).then((stg, vk) => {
+				stg.user_id = vk.user_id;
+				vk.api('execute.getdata_beta', stg).done(API => {
+					if (!this.delay) return;
 
-						if ( API.system && API.system.lastAlertId > params.lastLoadAlert ) {
-							vk.api('execute.getAlerts', {
-								'lang': API.lang,
-								'lastAlert': params.lastLoadAlert
-							}).done((loaded) => {
-								if ( !$.isEmptyObject(loaded.alert) ) {
-									chrome.storage.local.set({'lastLoadAlert': loaded.id});
-									this.saveAlert(loaded.alert);
-								}
-							});
-						}
+					API.lang = getLangCode(API.lang);
 
-						delete API.system;
-						chrome.storage.local.set(API);
-						chrome.browserAction.setIcon({path: 'img/icon38.png'});
-
-						this.setCounters(API.counter, API.dialogs).saveAlert(false, 'error');
-
-						this.firstRequest.resolve();
+					if (API.system && API.system.lastAlertId > stg.lastLoadAlert) {
+						vk.api('execute.getAlerts', {
+							'lang': API.lang,
+							'lastAlert': stg.lastLoadAlert
+						}).done(loaded => {
+							if (!$.isEmptyObject(loaded.alert)) {
+								chrome.storage.local.set({'lastLoadAlert': loaded.id});
+								this.saveAlert(loaded.alert);
+							}
+						});
 					}
+
+					delete API.system;
+					chrome.storage.local.set(API);
+					chrome.browserAction.setIcon({path: 'img/icon38.png'});
+					this.setCounters(API.counter, API.dialogs).saveAlert(false, 'error').firstRequest.resolve();
 				}).fail((code, details) => {
 					this.generateError(code, details);
 					this.badge = 0;
@@ -118,11 +86,11 @@ var Informer = (function () {
 					chrome.browserAction.setIcon({path: 'img/icon38-off.png'});
 					chrome.browserAction.setBadgeText({text: ''});
 				}).always(() => {
-					if ( this.delay > 0 ) {
+					if (this.delay > 0) {
 						setTimeout(() => this.mainRequest(), this.delay);
 					}
 				});
-			}, (code) => {
+			}, code => {
 				this.generateError(code).deamonStop();
 				chrome.browserAction.setIcon({path: 'img/icon38-off.png'});
 			});
@@ -139,22 +107,20 @@ var Informer = (function () {
 			 * Поле comments присутствует всегда, при условии, что ВК корректно вернул счетчики
 			 * Проверяем, существует ли данное поле. Если нет - значит счетчики не корректные и ничего не делаем.
 			 */
-			if ( typeof counters.comments === 'undefined' ) {
+			if (typeof counters.comments === 'undefined') {
 				return this;
 			}
 
-			if ( !$.isEmptyObject(counters) ) {
+			if (!$.isEmptyObject(counters)) {
 
-				new App().load({'showMessage': false}).done((stg) => {
+				new App().load({'showMessage': false}).done(stg => {
 					let sum = 0;
 					let needSound = false;
 					$.each(counters, (c, val) => {
-						if ( c === 'messages' && stg.showMessage && dialogs ) {
+						if (c === 'messages' && stg.showMessage && dialogs) {
 							sum = dialogs.reduce((sum, dialog) => {
-								if ( dialog.unread ) {
-									if ( !needSound && (dialog.push_settings && dialog.push_settings.sound) ) {
-										needSound = true;
-									}
+								if (dialog.unread) {
+									dialog.push_settings && dialog.push_settings.sound || (needSound = true);
 									return sum + dialog.unread;
 								} else {
 									return sum;
@@ -167,7 +133,7 @@ var Informer = (function () {
 					});
 
 					if (sum > this.badge && needSound) {
-						this.playSound();
+						playSound();
 					}
 					if (sum > 999) {
 						chrome.browserAction.setBadgeText({text: '999+'});
@@ -188,41 +154,16 @@ var Informer = (function () {
 		},
 
 		/**
-		 * Воспроизводит звук
-		 */
-		playSound: function () {
-			new App().load({'audio': true}).done(stg => {
-				if ( stg.audio === true ) {
-					chrome.tabs.query({
-						url: '*://vk.com/*'
-					}, tabs => {
-						if (tabs.every(tab => /vk.com\/(?:login.*)?$/i.test(tab.url))) {
-							$('#audio')[0].play();
-						}
-					});
-				}
-			});
-			return this;
-		},
-
-		/**
 		 * Сохраняет всплывающее сообщение
 		 * @param {String} type						Тип сообщения
 		 * @param {Object} alert_obj				Объект сообщения
-		 * @param {String} alert_obj.header			Текст заголовока
-		 * @param {String} alert_obj.footer			Текст ссылки "закрыть"
-		 * @param {Object} alert_obj.body			Объект тела
-		 * @param {String} alert_obj.body.text		Текст тела
-		 * @param {String} alert_obj.body.url		Адрес ссылки
-		 * @param {String} alert_obj.body.ancor		Текст ссылки
-		 * @param {String} alert_obj.body.img		Адрес изображения
 		 */
 		saveAlert: function (alert_obj, type) {
-			if ( alert_obj ) {
-				if ( !type ) type = 'message';
+			if (alert_obj) {
+				!type && (type = 'message');
 
 				chrome.storage.local.set({
-					[ `alert_${type}` ]:alert_obj
+					[`alert_${type}`]: alert_obj
 				});
 			} else {
 				chrome.storage.local.remove([`alert_${type}`]);
@@ -236,20 +177,15 @@ var Informer = (function () {
 		 * @param  {Object} details			Объект ошибки
 		 */
 		generateError: function (code, details) {
-			let alert = {
-				'header': 'Unknown error occurred',
-				'body': {
-					'text'	: 'Please inform the developers.',
-					'ancor'	: 'Login',
-					'url'	: new Vk().authUrl
-				}
-			};
+			const authUrl = new Vk().authUrl;
+			let alert = {};
+
 			switch (code) {
 				case 0 :	// Не удалось загрузить access_token или user_id
 					alert = {
 						'body': {
 							'ancor'	: 'Login',
-							'url'	: new Vk().authUrl
+							'url'	: authUrl
 						}
 					};
 				break;
@@ -266,19 +202,29 @@ var Informer = (function () {
 						'body': {
 							'text'	: `${code}/${details.error_code}. ${details.error_msg}`,
 							'ancor'	: 'Login',
-							'url'	: new Vk().authUrl
+							'url'	: authUrl
 						}
 					};
 
-					if ( $.inArray(details.error_code, [5, 7, 10, 15, 17, 113]) !== -1 ) {
+					if ($.inArray(details.error_code, [5, 7, 10, 15, 17, 113]) !== -1) {
 						alert.body.text = '';
 						alert.header = '';
-					} else if ( $.inArray(details.error_code, [6, 9]) !== -1 ) {
-						if ( this.deamonStop() ) {
+					} else if ($.inArray(details.error_code, [6, 9]) !== -1) {
+						if (this.deamonStop()) {
 							setTimeout(() => this.deamonStart(), 10000);
 						}
 						alert = false;
 					}
+				break;
+				default:
+					alert = {
+						'header': 'Unknown error occurred',
+						'body': {
+							'text'	: 'Please inform the developers.',
+							'ancor'	: 'Login',
+							'url'	: authUrl
+						}
+					};
 				break;
 			}
 
@@ -286,4 +232,43 @@ var Informer = (function () {
 			return this.saveAlert(alert, 'error');
 		}
 	};
+
+	/**
+	 * Вычисляет код языка
+	 * @param  {Number}	lang_code	Код языка для проверки
+	 * @return {Number}				Код поддерживаемого языка
+	 */
+	function getLangCode(lang_code) {
+		switch (lang_code) {
+			case 0: case 97: case 100: case 777:
+				return 0;  // Русский
+			case 1:
+				return 1;  // Украинский
+			case 2: case 114:
+				return 2;  // Белоруский
+			case 6:
+				return 6;  // Немецкий
+			case 15:
+				return 15; // Польский
+			case 54: case 66:
+				return 54; // Румунский
+			case 61:
+				return 61; // Нидерландский
+			default:
+				return 3;  // Английский
+		}
+	}
+
+	/**
+	 * Воспроизводит звук
+	 */
+	function playSound() {
+		new App().load({'audio': true}).done(stg => {
+			if (stg.audio) {
+				chrome.tabs.query({
+					url: '*://vk.com/*'
+				}, tabs => tabs.every(tab => /vk.com\/(?:login.*)?$/i.test(tab.url)) && $('#audio')[0].play());
+			}
+		});
+	}
 })();
