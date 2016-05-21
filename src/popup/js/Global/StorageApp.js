60ce4bdf67c38fef31cfd1aaf2b13e30e114a343
Alex Kozack <kozackunisoft@gmail.com>
2016-05-21 10:50:34 +0300

    Storage FilterFactory

--- src/popup/js/Global/StorageApp.js
+++ src/popup/js/Global/StorageApp.js
@@ -1,50 +1,69 @@
 angular.module('StorageApp', [])
-	.service('storage', ['$q','$rootScope', function ($q,$rootScope) {
-		let $this = this;
-		let defer = $q.defer();
-		$this.defer = defer.promise;
 
-		chrome.storage.local.get(function (stg) {
-			$this.stg = stg;
-			$this.stg.profiles = filter(stg.profiles, 'profiles');
-			$this.stg.friends = filter(stg.friends, 'friends');
-
-
-
-			$rootScope.$apply();
-			defer.resolve($this.stg)
-
-			chrome.storage.onChanged.addListener(function (changes) {
-				if (changes.profiles !== undefined) {
-					$this.stg.profiles = filter(changes.profiles.newValue, 'profiles');
-					delete changes.profiles;
-				}
-				angular.forEach(changes, function(val, key) {
-					$this.stg[key] = filter(val.newValue, key);
-				});
-				$rootScope.$apply();
-			});
-		});
-
-		function filter (val, key) {
-			switch (key) {
-				case 'profiles' : return index_users(val);
-				case 'friends' : return replace_id_to_user(val);
-				default : return val;
-			}
+	.factory('filter', [function () {
+		function auto_filter (val, key) {
+			if (this[key] !== undefined) return this[key](val);
+			else return default_filter(val);
 		}
 
-		function index_users(users) {
-			const indexed_list = angular.isArray(users) ? {} : users;
+		function index_users_filter(users, target) {
+			const indexed_list = angular.isArray(target) ? {} : angular.isObject(target) ? target : {};
 			angular.forEach(users, function (user) {
 				indexed_list[user.id] = user;
 			});
 			return indexed_list;
 		}
 
-		function replace_id_to_user (array_ids) {
+		function replace_id_to_user_filter (array_ids, profiles) {
 			return array_ids.map((user_id) => {
-				return $this.stg.profiles[user_id] || {};
+				return profiles[user_id] || {};
 			});
 		}
+
+		function default_filter (val) {
+			return val;
+		}
+
+		return {
+			auto: auto_filter,
+			profiles: index_users_filter,
+			friends: replace_id_to_user_filter
+		}
+
+	}])
+
+	.service('storage', ['$q','$rootScope','filter', function ($q,$rootScope,filter) {
+		let $this = this;
+		let defer = $q.defer();
+		$this.defer = defer.promise;
+
+		chrome.storage.local.get(function (stg) {
+			$this.stg = angular.extend({}, stg);
+
+			$this.stg.profiles = filter.profiles(stg.users, $this.stg.profiles);
+			$this.stg.profiles = filter.profiles(stg.groups, $this.stg.profiles);
+			$this.stg.friends = filter.friends(stg.friends, $this.stg.profiles);
+
+			delete $this.stg.users;
+			delete $this.stg.groups;
+
+			$rootScope.$apply();
+			defer.resolve($this.stg)
+
+			chrome.storage.onChanged.addListener(function (changes) {
+				if (changes.users !== undefined) {
+					$this.stg.profiles = filter.profiles(changes.users.newValue, $this.stg.profiles);
+					delete changes.users;
+				}
+				if (changes.groups !== undefined) {
+					$this.stg.profiles = filter.profiles(changes.groups.newValue, $this.stg.profiles);
+					delete changes.groups;
+				}
+				angular.forEach(changes, function(val, key) {
+					$this.stg[key] = filter.auto(val.newValue, key);
+				});
+				$rootScope.$apply();
+			});
+		});
+
 	}]);
