e6312bfb7bc0a25bcf41cd647fdcf513817c9e54
Alex Kozack <kozackunisoft@gmail.com>
2016-05-23 19:11:49 +0300

    Добавлены переводы

--- src/popup/js/PopupApp.js
+++ src/popup/js/PopupApp.js
@@ -1,10 +1,34 @@
 angular.module('PopupApp', ['HeaderApp', 'SectionsApp', 'gettext'])
 .run(function (gettextCatalog, storage) {
 
-    gettextCatalog.setCurrentLanguage('uk');
 	gettextCatalog.debug = true;
 
 	storage.defer.then(function (stg) {
-		console.log('PopupApp.js', stg);
+		const lang = getLang(stg.lang);
+		console.log('PopupApp.js', lang);
+		if (lang !== 'ru') {
+			gettextCatalog.setCurrentLanguage(lang);
+		}
+
+		function getLang(lang_code) {
+			switch (lang_code) {
+				case 0: case 97: case 100: case 777:
+					return 'ru';  // Русский
+				case 1:
+					return 'uk';  // Украинский
+				case 2: case 114:
+					return 'be';  // Белоруский
+				case 6:
+					return 'de';  // Немецкий
+				case 15:
+					return 'pl'; // Польский
+				case 54: case 66:
+					return 'ro'; // Румунский
+				case 61:
+					return 'nl'; // Нидерландский
+				default:
+					return 'en';  // Английский
+			}
+		}
 	});
 });
