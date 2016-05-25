6efc4171bf3a0200659d4173959bcae5a04a1940
Alex Kozack <kozackunisoft@gmail.com>
2016-05-25 12:19:53 +0300

    Исправления структуры в Popup

--- src/SectionApp/js/SectionsApp.js
+++ src/SectionApp/js/SectionsApp.js
@@ -1,7 +1,6 @@
 angular.module('SectionsApp', [])
 	.constant('sections', {
-		'default':    'js/Default/DefaultTempl.html',
-		'messages':   'js/NewMess/NewMessTempl.html',
-		'history':    'js/History/HistoryTempl.html',
-		'friends': 	  'js/NewFriends/NewFriendsTempl.html',
+		'default':    '../SectionApp/sections/Default/DefaultTempl.html',
+		'friends': 	  '../SectionApp/sections/NewFriends/NewFriendsTempl.html',
+		'messages':   '../SectionApp/sections/NewMess/NewMessTempl.html',
 	});
