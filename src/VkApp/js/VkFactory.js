d69ffca45873dd31e89cea5307fa37b4051add1a
Alex Kozack <kozackunisoft@gmail.com>
2016-05-30 00:21:33 +0300

    Рефакторинг VkApp

--- src/VkApp/js/VkFactory.js
+++ src/VkApp/js/VkFactory.js
@@ -7,34 +7,39 @@ angular.module('VkApp')
 .factory('$vk', ['$q','$http','$httpParamSerializer', 'storage', 'apiConfig', '$log',
 function ($q, $http,$httpParamSerializer, storage, apiConfig, $log) {
 	return {
-		authUrl: 'https://oauth.vk.com/authorize?' + $httpParamSerializer({
-			'redirect_uri'	: 'https://oauth.vk.com/blank.html',
-			'client_id'		: 4682781,
-			'scope'			: 'offline,friends,messages,notifications,wall',
-			'response_type'	: 'token',
-			'display'		: 'popup',
-			'v'				: apiConfig.version,
-			'state'			: 'vknotice',
+		authUrl:          'https://oauth.vk.com/authorize?' + $httpParamSerializer({
+			'redirect_uri':  'https://oauth.vk.com/blank.html',
+			'client_id':     4682781,
+			'scope':         'offline,friends,messages,notifications,wall',
+			'response_type': 'token',
+			'display':       'popup',
+			'v':             apiConfig.version,
+			'state':         'vknotice',
 		}),
 
+
 		isAuth: function () {
+			const $vk   = this;
 			const ready = $q.defer();
-			const $vk = this;
-			$vk.api('users.get', {access_token: $vk.stg.access_token}).then(function (resp) {
-				ready.resolve(resp && resp[0] && resp[0].id && resp[0].id == $vk.stg.user_id);
-			}, function () {
+			if ($vk.stg.access_token && $vk.stg.user_id) {
+				$vk.api('users.get', {access_token: $vk.stg.access_token}).then(function (resp) {
+					ready.resolve(resp && resp[0] && resp[0].id && resp[0].id == $vk.stg.user_id);
+				}, function () {
+					ready.resolve(false);
+				});
+			} else {
 				ready.resolve(false);
-			});
+			}
 			return ready.promise;
 		},
 
 		auth: function () {
+			const $vk   = this;
 			const ready = $q.defer();
-			const $vk = this;
 			storage.ready.then(function (stg) {
 				$vk.stg = stg;
 				$vk.isAuth().then(function (isAuth) {
-					if ( isAuth ) {
+					if (isAuth) {
 						ready.resolve();
 					} else {
 						chrome.tabs.create({url: $vk.authUrl, active: true}, function (tab) {
@@ -45,11 +50,11 @@ function ($q, $http,$httpParamSerializer, storage, apiConfig, $log) {
 									&& changeInfo.url.indexOf('oauth.vk.com/blank.html') > -1
 								) {
 									let authData = $vk.parseHashParams(changeInfo.url);
+									$vk.stg.user_id      = authData.user_id;
 									$vk.stg.access_token = authData.access_token;
-									$vk.stg.user_id = authData.user_id;
 
 									storage.set({
-										user_id: $vk.stg.user_id,
+										user_id:      $vk.stg.user_id,
 										access_token: $vk.stg.access_token,
 									});
 
@@ -60,32 +65,24 @@ function ($q, $http,$httpParamSerializer, storage, apiConfig, $log) {
 							chrome.tabs.onRemoved.addListener(function tabRemovedListener (tabId) {
 								if (authTabId === tabId) {
 									$vk.isAuth().then(function (isAuth) {
-										if ( isAuth ) {
-											ready.resolve();
-										} else {
-											ready.reject();
-										}
+										ready[ isAuth ? 'resolve' : 'reject' ]();
 										chrome.tabs.onRemoved.removeListener(tabRemovedListener);
 									});
 								}
-
 							});
 						});
 					}
 				});
-
 			});
 
 			return ready.promise;
 		},
 
 		parseHashParams: function (url) {
-			const ret = {};
-
+			const ret  = {};
 			const hash = url.split('#')[1].split('&');
 			for (let i = hash.length; i--;) {
 				if (!hash[i]) continue;
-
 				let s = hash[i].split('=');
 				ret[s[0]] = s[1];
 			}
