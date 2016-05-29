68fa31f336adbb5f266dda8f23adb29d123813ba
Alex Kozack <kozackunisoft@gmail.com>
2016-05-29 18:39:24 +0300

    Linting PopupApp

--- src/PopupApp/js/PopupApp.js
+++ src/PopupApp/js/PopupApp.js
@@ -1,34 +1,35 @@
 angular.module('PopupApp', ['HeaderApp', 'SectionsApp', 'gettext'])
-.run(function (gettextCatalog, storage) {
+.run(['gettextCatalog', 'storage', '$log',
+function (gettextCatalog, storage, $log) {
 
 	gettextCatalog.debug = true;
 
 	storage.ready.then(function (stg) {
-		const lang = getLang(stg.lang);
+		var lang = getLang(stg.lang);
 		if (lang !== 'ru') {
-			console.info('Current Language set as "' + lang + '"');
+			$log.info('Current Language set as "' + lang + '"');
 			gettextCatalog.setCurrentLanguage(lang);
 		}
 
 		function getLang(lang_code) {
 			switch (lang_code) {
-				case 0: case 97: case 100: case 777:
-					return 'ru';  // Русский
-				case 1:
-					return 'uk';  // Украинский
-				case 2: case 114:
-					return 'be';  // Белорусский
-				case 6:
-					return 'de';  // Немецкий
-				case 15:
-					return 'pl'; // Польский
-				case 54: case 66:
-					return 'ro'; // Румынский
-				case 61:
-					return 'nl'; // Нидерландский
-				default:
-					return 'en';  // Английский
+			case 0: case 97: case 100: case 777:
+				return 'ru';  // Русский
+			case 1:
+				return 'uk';  // Украинский
+			case 2: case 114:
+				return 'be';  // Белорусский
+			case 6:
+				return 'de';  // Немецкий
+			case 15:
+				return 'pl'; // Польский
+			case 54: case 66:
+				return 'ro'; // Румынский
+			case 61:
+				return 'nl'; // Нидерландский
+			default:
+				return 'en';  // Английский
 			}
 		}
 	});
-});
+}]);
--- src/PopupApp/js/PreloaderDirective.js
+++ src/PopupApp/js/PreloaderDirective.js
@@ -2,5 +2,5 @@ angular.module('PopupApp')
 	.directive('preloader', function () {
 		return {
 			template: '<div class="spinner"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div>',
-		}
-	})
+		};
+	});
