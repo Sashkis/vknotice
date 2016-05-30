4843b44669921fa18c87bfdd8a26e45f1bb4df51
Alex Kozack <kozackunisoft@gmail.com>
2016-05-30 12:51:30 +0300

    Formated HeaderApp

--- src/HeaderApp/js/HeaderCtrl.js
+++ src/HeaderApp/js/HeaderCtrl.js
@@ -2,7 +2,7 @@ angular.module('HeaderApp')
 
 	.config([
 		'$compileProvider',
-		function($compileProvider) {
+		function ($compileProvider) {
 			// Разрешить ссылки chrome-extension://
 			$compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension):/);
 		},
@@ -16,22 +16,24 @@ angular.module('HeaderApp')
 		'$httpParamSerializer',
 		function ($log, storage, profile, gettextCatalog, $httpParamSerializer) {
 			const vm = this;
+
 			vm.isDropdownOpen = false;
 
 			vm.getShareUrl = function () {
 				return 'https://vk.com/share.php?' + $httpParamSerializer({
 					'url'         : 'http://vk.com/note45421694_12011424',
 					'title'       : gettextCatalog.getString('Информер Вконтакте'),
+					// eslint-disable-next-line max-len
 					'description' : gettextCatalog.getString('Отображает количество непрочитанных сообщений и позволяет ответить не заходя в ВК!'),
 					'image'       : 'https://pp.vk.me/c628716/v628716694/2c20c/f3gq0pcaqHI.jpg',
 					'noparse'     : 'true',
 				});
 			};
 
-			vm.getReviewUrl = function () {
-				return /(opera|opr|Yandex|YaBrowser)/i.test(navigator.userAgent) ?
-				('https://addons.opera.com/extensions/details/app_id/ephejldckfopeihjfhfajiflkjkjbnin#feedback-container') :
-				('https://chrome.google.com/webstore/detail/jlokilojbcmfijbgbioojlnhejhnikhn/reviews');
+			vm.getReviewUrldkfsnd = function () {
+				return /(opera|opr|Yandex|YaBrowser)/i.test(navigator.userAgent)
+				? ('https://addons.opera.com/extensions/details/app_id/ephejldckfopeihjfhfajiflkjkjbnin#feedback-container')
+				: ('https://chrome.google.com/webstore/detail/jlokilojbcmfijbgbioojlnhejhnikhn/reviews');
 			};
 
 			vm.logout = function  ($event) {
