4c9c77897227c6a0c5c3699b4eac80418efd7413
Alex Kozack <kozackunisoft@gmail.com>
2016-07-05 16:14:27 +0300

    Add chrome-app.d.ts

--- src/all.d.ts
+++ src/all.d.ts
@@ -1,4 +1,5 @@
 /// <reference path="./libs/chrome/chrome.d.ts"/>
+/// <reference path="./libs/chrome/chrome-app.d.ts"/>
 /// <reference path='./libs/angular/angular.d.ts' />
 /// <reference path="./libs/vk/openapi.d.ts"/>
 /// <reference path="./StorageApp/storage.d.ts"/>
