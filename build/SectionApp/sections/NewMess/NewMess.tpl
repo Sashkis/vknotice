c7edd31440dffa2e18f4f1898d71f8ed95ffad0e
Alex Kozack <kozackunisoft@gmail.com>
2016-07-06 17:47:54 +0300

    Мелкие изменения в сборке

--- build/SectionApp/sections/NewMess/NewMess.tpl
+++ build/SectionApp/sections/NewMess/NewMess.tpl
@@ -1,1 +1,6 @@
-<div class="" ng-controller="NewMessCtrl as dialogs"><div ng-repeat="dialog in dialogs.stg.dialogs">{{dialog.message.body}}<br></div></div>
+<div class="" ng-controller="NewMessCtrl as dialogs">
+  <div ng-repeat="dialog in dialogs.stg.dialogs">
+    {{dialog.message.body}}
+    <br>
+  </div>
+</div>
--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,10 @@ var filter     = require('gulp-filter');
 
 var sass       = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
-// var gulpif     = require('gulp-if');
-var flatten    = require('gulp-flatten');
 
 var useref     = require('gulp-useref');
 var uglify     = require('gulp-uglify');
 var cleanCSS   = require('gulp-clean-css');
-var htmlmin    = require('gulp-htmlmin');
 var jsonminify = require('gulp-jsonminify');
 
 var wiredep    = require('wiredep').stream;
@@ -51,38 +48,26 @@ gulp.task('sass:watch', ['sass'], function () {
 });
 
 
-gulp.task('copy', ['copy:fonts'], function () {
+gulp.task('copy', ['clean'], function () {
+
+	gulp.src(['src/bower_components/roboto-font/fonts/*.woff2',
+	          'src/bower_components/components-font-awesome/fonts/*.woff2'])
+			 .pipe(gulp.dest('build/vendors/fonts'))
+
 	var json = filter(['**/*.json'], {restore: true});
-	var html = filter(['**/*.tpl'], {restore: true});
 	return gulp.src(['src/*.json', 'src/+(_locales)/**/*.json', 'src/+(BackgroundApp)/+(audio|img)/*', 'src/**/*.tpl'])
 				.pipe(json).pipe(jsonminify()).pipe(json.restore)
-				.pipe(html).pipe(htmlmin({collapseWhitespace: true})).pipe(html.restore)
 	      .pipe(gulp.dest('build'))
 });
 
-gulp.task('copy:fonts', function () {
-	var fileName = '*.{woff,woff2}';
-	return gulp.src([
-		'src/bower_components/open-sans/fonts/+(regular|bold)/'+fileName,
-		'src/bower_components/components-font-awesome/fonts/'+fileName,
-	])
-	      .pipe(flatten({ includeParents: -1}))
-	      .pipe(gulp.dest('build/vendors/fonts'));
-});
+gulp.task('build', ['copy', 'sass'], function () {
 
-
-gulp.task('build', ['clean'], function () {
-	gulp.start('build:source')
-});
-gulp.task('build:source', ['copy', 'sass'], function () {
 	var js = filter(['**/*.js'], {restore: true});
 	var css = filter(['**/*.css'], {restore: true});
-	var html = filter(['**/*.html'], {restore: true});
 	return gulp.src(['src/+(OptionsApp|BackgroundApp|PopupApp)/*.html'])
 				.pipe(useref())
 	      .pipe(js).pipe(uglify()).pipe(js.restore)
 	      .pipe(css).pipe(cleanCSS()).pipe(css.restore)
-	      .pipe(html).pipe(htmlmin({collapseWhitespace: true})).pipe(html.restore)
 				.pipe(gulp.dest('build'))
 				;
 });
