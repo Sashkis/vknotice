ec43ab04f51ced8940ad92579f46b2ff7539042d
Alex Kozack <kozackunisoft@gmail.com>
2016-07-15 11:45:51 +0300

    Install Angular-Route

--- src/all.d.ts
+++ src/all.d.ts
@@ -1,5 +1,6 @@
 /// <reference path="./libs/chrome/chrome.d.ts"/>
 /// <reference path='./libs/angular/angular.d.ts' />
+/// <reference path="./libs/angular/angular-route.d.ts"/>
 /// <reference path="./libs/vk/openapi.d.ts"/>
 /// <reference path="./StorageApp/storage.d.ts"/>
 /// <reference path="./VkApp/js/VkService.ts"/>
