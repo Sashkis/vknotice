3e7599d52f1a12f39c29c50dbd09a7d166403482
Alex Kozack <kozackunisoft@gmail.com>
2016-07-27 20:01:18 +0300

    Исправлены чекбоксы на странице настроек

--- src/OptionsApp/js/OptionsApp.js
+++ src/OptionsApp/js/OptionsApp.js
@@ -5,6 +5,13 @@ var OptionsApp;
         .run(['gettextCatalog', 'storage', Helpers.setCurrentLanguage])
         .run(['Analytics', 'storage', Helpers.trackPage])
         .controller('OptionsCtrl', OptionsApp.OptionsCtrl)
-        .directive('option', function () { return new OptionsApp.OptionsDirective; });
+        .directive('checkboxOption', function () {
+        return {
+            template: "<input ng-model=\"param\" type=\"checkbox\"/>\n\t\t\t\t\t\t\t\t\t <span class=\"fa-stack\">\n\t\t\t\t\t\t\t\t\t\t<i class=\"fa fa-square fa-stack-2x\"></i>\n\t\t\t\t\t\t\t\t\t\t<i ng-if=\"param\" class=\"fa fa-check fa-stack-1x fa-inverse\"></i>\n\t\t\t\t\t\t\t\t\t </span>",
+            scope: {
+                param: '=?'
+            }
+        };
+    });
 })(OptionsApp || (OptionsApp = {}));
 //# sourceMappingURL=OptionsApp.js.map
--- src/OptionsApp/js/OptionsApp.ts
+++ src/OptionsApp/js/OptionsApp.ts
@@ -4,5 +4,16 @@ module OptionsApp {
 		.run(['gettextCatalog', 'storage', Helpers.setCurrentLanguage])
 		.run(['Analytics', 'storage', Helpers.trackPage])
 		.controller('OptionsCtrl', OptionsCtrl)
-		.directive('option', () => new OptionsDirective);
+		.directive('checkboxOption', () => {
+			return {
+				template: `<input ng-model="param" type="checkbox"/>
+									 <span class="fa-stack">
+										<i class="fa fa-square fa-stack-2x"></i>
+										<i ng-if="param" class="fa fa-check fa-stack-1x fa-inverse"></i>
+									 </span>`,
+				scope: {
+					param: '=?'
+				}
+			}
+		});
 }
