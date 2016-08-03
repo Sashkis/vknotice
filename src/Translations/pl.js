ab4e11a8581ef08b320bab1bd7b2ce676c3574d7
Alex Kozack <kozackunisoft@gmail.com>
2016-08-03 20:36:45 +0300

    Обновлены переводы для английского и польского языков

--- src/Translations/pl.js
+++ src/Translations/pl.js
@@ -1,5 +1,5 @@
 angular.module('gettext').run(['gettextCatalog', function (gettextCatalog) {
 /* jshint -W100 */
-    gettextCatalog.setStrings('pl', {});
+    gettextCatalog.setStrings('pl', {"Аудиозаписи":"Pliki audio","Введите ваше сообщение":"Wprowadź wiadomość tutaj","Видеозаписи":"Pliki Wideo","Группы":"Grupy","Друзья":"Znajomi","Комментарии":"Komentarze","Комментарий":"Komentarz","Моя Страница":"Moja strona","Настройки":"Ustawienia","Новости":"Nowości","Фотографии":"Zdjęcia"});
 /* jshint +W100 */
 }]);
